1bc29cfe1d520bb5b3eea85b83048c1bf801a5b1
Christopher Andrews <godarklight@gmail.com>
2020-06-19 09:49:46 +1000

    Add DecalTool and ActionFigureTool patch

---
+++ Balsa/ActionFigureTool.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace Balsa
+{
+    public class ActionFigureTools : PatchInterface
+    {
+        public string GetName()
+        {
+            return "ActionFigureTools";
+        }
+
+        private MethodDefinition GetMethodDefinition(AssemblyDefinition assembly)
+        {
+            return assembly.MainModule.Types.First(type => type.FullName == "PlayerTools.ActionFigureTool").Resolve().Methods.First(method => method.Name == "GetToolAvailable");
+        }
+
+        public bool Applied(AssemblyDefinition assembly)
+        {
+            MethodDefinition toolAvailable = GetMethodDefinition(assembly);
+            return toolAvailable.Body.Instructions[0].OpCode == OpCodes.Ldc_I4_1;
+        }
+
+        public bool Patch(AssemblyDefinition assembly)
+        {
+            MethodDefinition toolAvailable = GetMethodDefinition(assembly);
+            ILProcessor processor = toolAvailable.Body.GetILProcessor();
+            Instruction oldI = toolAvailable.Body.Instructions[0];
+            Instruction newI = processor.Create(OpCodes.Ldc_I4_1);
+            processor.Replace(oldI, newI);
+            return true;
+        }
+    }
+}
---
+++ Balsa/DecalTool.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace Balsa
+{
+    public class DecalTool : PatchInterface
+    {
+        public string GetName()
+        {
+            return "DecalTool";
+        }
+
+        private TypeDefinition GetTypeDefinition(AssemblyDefinition assembly)
+        {
+            return assembly.MainModule.Types.First(type => type.FullName == "Construction.DecalTool").Resolve();
+        }
+
+        public bool Applied(AssemblyDefinition assembly)
+        {
+            TypeDefinition decalTool = GetTypeDefinition(assembly);
+            CustomAttribute decalAttribute = decalTool.CustomAttributes[0];
+            return (int)decalAttribute.Fields.First(field => field.Name == "visible").Argument.Value == 0;
+        }
+
+        public bool Patch(AssemblyDefinition assembly)
+        {
+            TypeDefinition decalTool = GetTypeDefinition(assembly);
+            CustomAttribute decalAttribute = decalTool.CustomAttributes[0];
+            TypeDefinition whenType = decalAttribute.AttributeType.Resolve().NestedTypes[0];
+            decalAttribute.Fields.Remove(decalAttribute.Fields.First(canaTest => canaTest.Name == "visible"));
+            CustomAttributeNamedArgument cana = new CustomAttributeNamedArgument("visible", new CustomAttributeArgument(whenType, 0));
+            decalAttribute.Fields.Add(cana);
+            return true;
+        }
+    }
+}
